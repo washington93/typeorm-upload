deebe4e7b859394bfd018b681656d52f79ecf021
Washington Silva <washington.s@escolar.ifrn.edu.br>
2020-08-03 16:38:25 -0300

    Transactions: Create, read, update, delete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
-interface Balance {
+interface IBalance {
   income: number;
   outcome: number;
   total: number;
@@ -10,8 +10,30 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+  public async getBalance(): Promise<IBalance> {
+    const transactions = await this.find()
+
+    const {income, outcome} =  transactions.reduce((acumulator, transaction) => {
+      switch(transaction.type){
+        case 'income':
+          acumulator.income += Number(transaction.value)
+          break
+        case 'outcome':
+          acumulator.outcome += Number(transaction.value)
+          break
+        default:
+          break
+      }
+      return acumulator
+    }, {
+      income:0,
+      outcome: 0,
+      total: 0
+    })
+
+    const total = income - outcome
+
+    return {income, outcome, total}
   }
 }
 
